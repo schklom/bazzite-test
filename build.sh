fa3a72ebd3211e3f0126cfb72edd027224d00790
schklom <23263086+schklom@users.noreply.github.com>
2024-07-08 14:37:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,96 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install \
+    distrobox \ # run any os in containers easily 
+    htop \ # Better "top" 
+    ffmpeg \
+    kdepim-addons \ # For the Event Calendar widget, to work with external calendars 
+    kde-connect \
+    kdeconnectd \
+    kontact \ # Calendar + Contact + Task manager. If installed via Flatpak, it will not work with Event  Calendar.
+    libvirt \ # Run VMs 
+    mono-complete \ # KeePass 
+    mozilla-openh264 \ # Video support on Firefox 
+    podman-compose \ # Use docker.compose.yml files with podman 
+    plasma-workspace-x11 \ # Fedora decided to remove X11, so I have to add it back 
+    usbguard \ # Protect against unknown USB devices 
+    rpmfusion-free-release \
+    rpmfusion-nonfree-release \
+    virt-manager \ # Run VMs 
+    xbindkeys \ # Shortcuts 
+    xdotool \ # KeePass 
+    xinput \ # KeePass
+
+rpm-ostree install \
+    \ # --add-repo=https://copr.fedorainfracloud.org/coprs/atim/starship/repo/fedora-%OS_VERSION%/atim-starship-fedora-%OS_VERSION%.repo \
+    igt-gpu-tools \ # To check GPU usage via sudo intel_gpu_top
+    intel-media-driver # Hardware acceleration with Intel (c.f. https://wiki.archlinux.org/title/Hardware_video_acceleration)
+
+rpm-ostree uninstall \
+    firefox \
+    noopenh264
+
+flatpak install -y \
+    \ # Browsers
+    org.mozilla.firefox \
+    io.github.ungoogled_software.ungoogled_chromium \
+    org.torproject.torbrowser-launcher \
+    \
+    \ # Media
+    org.libreoffice.LibreOffice \
+    org.kde.okular \
+    org.kde.kwrite \
+    org.videolan.VLC \
+    com.stremio.Stremio \
+    \ # com.obsproject.Studio
+    com.github.iwalton3.jellyfin-media-player \
+    org.gnome.Shotwell \
+    org.kde.gwenview \
+    tv.kodi.Kodi \
+    \ # org.clementine_player.Clementine
+    net.sapples.LiveCaptions \
+    \ # com.github.marinm.songrec
+    \
+    \ # Network
+    com.protonvpn.www \
+    org.jdownloader.JDownloader \
+    org.kde.ktorrent \
+    org.kde.krdc \
+    \
+    \ # Social
+    org.mozilla.Thunderbird \
+    \ # eu.betterbird.Betterbird
+    com.skype.Client \
+    com.github.IsmaelMartinez.teams_for_linux \
+    org.signal.Signal \
+    io.github.mahmoudbahaa.outlook_for_linux \
+    com.slack.Slack \
+    com.discordapp.Discord \
+    us.zoom.Zoom \
+    \
+    \ # Developer tools
+    com.vscodium.codium \
+    org.fedoraproject.MediaWriter \
+    org.kde.isoimagewriter \
+    \ # com.github.tchx84.Flatseal
+    \
+    \ # Others
+    com.valvesoftware.Steam \
+    net.lutris.Lutris \
+    com.usebottles.bottles \
+    com.dropbox.Client \
+    org.zotero.Zotero \
+    \ # org.gnome.OCRFeeder
+    org.gnome.Calculator \
+    io.github.prateekmedia.appimagepool \
+    dev.lasheen.qr \
+    org.kde.krename \
+    xyz.xclicker.xclicker \
+    com.nextcloud.desktopclient.nextcloud
+
+mkdir -p /etc/systemd/journald.conf.d/
+echo 'SystemMaxUse=400M' > /etc/systemd/journald.conf.d/max_size.conf
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
